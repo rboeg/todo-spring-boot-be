eeb5e3e962acda75f7d4c91e2144b9d89ea1334a
rboeg <renatoboegeholz@gmail.com>
2021-04-14 18:28:24 -0400

    Initial commit (pomxml)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # todo-spring-boot-be
-To-do Web application Spring Boot back end
+To-do Web application Spring Boot backend
---
+++ src/test/java/com/todoapp/be/TodoAppTests.java
@@ -0,0 +1,13 @@
+package com.todoapp.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
